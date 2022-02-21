7258841abb554cf92c5f581b9eebd24740176aea
Kelly Bradley <98112140+kelbrad7@users.noreply.github.com>
2022-02-21 17:04:10 -0600

    Add files via upload

---
+++ Exercise3.3/main.js
@@ -0,0 +1,8 @@
+
+const dogNames = {b:"Zoe", c:"Maggie",a:"Ruby"}
+const keys = Object.keys (dogNames)
+keys.sort();
+const values =Object.values (dogNames)
+values.sort();
+console.log (keys);
+console.log(values);
