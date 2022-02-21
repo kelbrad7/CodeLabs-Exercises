794724a9a756f2b9170938f0773c2a8b65b1aabf
Kelly Bradley <98112140+kelbrad7@users.noreply.github.com>
2022-02-21 17:10:52 -0600

    Delete Exercise3.3 directory

--- Exercise3.3/main.js
+++
@@ -1,8 +0,0 @@
-
-const dogNames = {b:"Zoe", c:"Maggie",a:"Ruby"}
-const keys = Object.keys (dogNames)
-keys.sort();
-const values =Object.values (dogNames)
-values.sort();
-console.log (keys);
-console.log(values);
