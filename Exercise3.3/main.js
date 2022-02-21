92337467855e42e3b4b4c325077f9682e1d6725d
Kelly Bradley <98112140+kelbrad7@users.noreply.github.com>
2022-02-21 17:22:12 -0600

    Update main.js

--- Exercise3.3/main.js
+++ Exercise3.3/main.js
@@ -1,9 +1,9 @@
 
 const dogNames = {b:"Zoe", c:"Maggie",a:"Ruby"}
 const keys = Object.keys (dogNames)
+const values =Object.values (dogNames)
 function sortObject(){
 keys.sort();
-const values =Object.values (dogNames)
 values.sort();
 console.log (keys);
 console.log(values);}
