b712d6746b285b8cfb514f34dea9dfd26c2b7c81
kelbrad7 <98112140+kelbrad7@users.noreply.github.com>
2022-03-04 13:55:50 -0600

    working version of 5.1

--- Exercise5.1/main.js
+++ Exercise5.1/main.js
@@ -2,45 +2,86 @@ const addNotes = document.querySelector ("#add_notes");
 const cssRadio = document.querySelector("#css_radio")
 const jsRadio = document.querySelector ("#js_radio");
 const form = document.querySelector ("#notes_form");
-
+    
+     
 //add event listener
 
 form.addEventListener ("submit", checkRadios);
-form.addEventListener ("submit", createSnip);
-//form.addEventListener ("submit", storeSnipsArray);
+form.addEventListener ("submit", storeSnipsArray);
 
 
-//function to createSnip
-function checkRadios(e) {
+//***function to check if radio has been selected***//
+function checkRadios() {
     
     if (cssRadio.checked == false && jsRadio.checked == false) {
         alert("Please choose CSS or JS");
-        e.preventDefault();}
+       
+    
+    }}
+     //*** list variables ***//
+     let cssNote = "";
+
+     let jsNote = "";
+
+     const listCS = document.querySelector("#css_ul");
+
+     const listJS = document.querySelector("#js_ul");
+
+    const snipsCSS = JSON.parse(localStorage.getItem("CSS")) || [];
+
+    const snipsJS = JSON.parse(localStorage.getItem("JS")) || [];
+
+    //*** grab value***//
+ function storeSnipsArray(e) {
+     e.preventDefault();
+    if (cssRadio.checked==true) {
+        cssNote = addNotes.value;
+    }
+        else if (jsRadio.checked== true) {
+            jsNote = addNotes.value;
+    } 
+    
+    //** push to local storage, append to list ***//
+    if (cssRadio.checked==true) {
+            snipsCSS.push(cssNote);
+            const newCSS = document.createElement ("li");
+            newCSS.innerHTML = cssNote;
+            newCSS.className = "note";
+            listCS.appendChild(newCSS);
+            localStorage.setItem("CSS",JSON.stringify(snipsCSS))
+        }
+        else if (jsRadio.checked== true) {
+            snipsJS.push(jsNote);
+            const newJS = document.createElement ("li");
+            newJS.innerHTML = jsNote;
+            newJS.className = "note";
+            listJS.appendChild(newJS);
+           localStorage.setItem("JS",JSON.stringify(snipsJS))
+        }
+            form.reset();
+        }        
+
+        //*** populate lists on page load ***/
+     function displayList () {
+       
+         for (let snipC of snipsCSS){
+            const itemCSS = document.createElement("li");
+            itemCSS.innerHTML = snipC;
+            itemCSS.className= "note"
+        listCS.appendChild(itemCSS);}
+
+            for(let snipJ of snipsJS ) {
+                const itemJS = document.createElement("li");
+                itemJS.innerHTML = snipJ;
+                itemJS.className = "note"
+            listJS.appendChild(itemJS);}
+
+             localStorage.clear();
+            }
+
+    
+        
         
 
-    //console.log("checkRadios");
-}
-
-let snip = "";
-function createSnip() {
-     snip = addNotes.value;
-     const snipsCSS =[];
-    //let snipsJS =[];
-     //if (cssRadio.checked==true){ 
-        snipsCSS.push(snip);
-        console.log(snipsCSS)};
-     
-    //console.log (snip);}
-
-
- //function storeSnipsArray(){
-     
     
-       // localStorage.setItem("CSS",JSON.stringify(snipsCSS));}
-    // else if (jsRadio.checked == true) {
-    //     snipsJS.push(snip);
-    //     localStorage.setItem("JS",JSON.stringify(snipsJS))};}
-    
-
-
-
+ 
